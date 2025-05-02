6d1ccaf4a805aa67aab0598d01094f7bbd8e5b74
Jamesssssss123456 <yap175028@gmail.com>
2025-05-02 12:33:47 +0800

    Update model_predictor.py

--- model_predictor.py
+++ model_predictor.py
@@ -1,1 +1,5 @@
-# Prediction logic using ai_soros_model.pkl
+import joblib
+
+def load_model_and_predict(features):
+    model = joblib.load("ai_soros_model.pkl")
+    return model.predict([features])[0]
