bb0ed703baacf21c6c9e32868f56703c3b9883ea
Jamesssssss123456 <yap175028@gmail.com>
2025-05-02 12:19:25 +0800

    Add files via upload

--- model_predictor.py
+++ model_predictor.py
@@ -1,5 +1,1 @@
-import joblib
-
-def load_model_and_predict(features):
-    model = joblib.load("ai_soros_model.pkl")
-    return model.predict([features])[0]
+# Prediction logic using ai_soros_model.pkl
